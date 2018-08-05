b63c53a52408baa73137cb4062bea7e7fa3142ee
LambertWu <26795806@qq.com>
2018-08-05 23:20:31 +0800

    Add NLog

--- samples/FogDemo.WebApi/Controllers/ValuesController.cs
+++ samples/FogDemo.WebApi/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace FogDemo.WebApi.Controllers
 {
@@ -10,10 +11,18 @@ namespace FogDemo.WebApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            _logger.LogInformation("Get method start");
             return new string[] { "value1", "value2" };
         }
 
--- samples/FogDemo.WebApi/Program.cs
+++ samples/FogDemo.WebApi/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace FogDemo.WebApi
 {
@@ -9,16 +11,39 @@ namespace FogDemo.WebApi
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
 
-            using (var scope = host.Services.CreateScope())
+            try
             {
-                var services = scope.ServiceProvider;
-                var dbContext = (MyDbContext)services.GetService(typeof(MyDbContext));
-                dbContext.Database.EnsureCreated();
+                logger.Debug("init main");
+
+                var host = CreateWebHostBuilder(args)
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    })
+                    .UseNLog()
+                    .Build();
+
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    var dbContext = (MyDbContext)services.GetService(typeof(MyDbContext));
+                    dbContext.Database.EnsureCreated();
+                }
+
+                host.Run();
             }
-            
-            host.Run();
+            catch (System.Exception ex)
+            {
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
